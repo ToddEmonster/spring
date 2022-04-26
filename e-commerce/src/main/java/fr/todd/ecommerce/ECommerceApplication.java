a1d2f9503c1b6577093c78aa4bb3173f65b7d256
Todd Raharivelo <t.raharivelo@outlook.fr>
2022-04-26 11:30:25 +0200

    fix: services xml works

--- e-commerce/src/main/java/fr/todd/ecommerce/ECommerceApplication.java
+++ e-commerce/src/main/java/fr/todd/ecommerce/ECommerceApplication.java
@@ -1,6 +1,5 @@
 package fr.todd.ecommerce;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
