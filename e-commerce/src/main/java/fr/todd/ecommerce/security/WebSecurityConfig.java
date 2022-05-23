f2325314cf8e7e21c7567570cd4fe16824460ce5
Todd Raharivelo <t.raharivelo@outlook.fr>
2022-05-23 12:08:41 +0200

    fix: add autowired for ClientService to UserDetailsService

--- e-commerce/src/main/java/fr/todd/ecommerce/security/WebSecurityConfig.java
+++ e-commerce/src/main/java/fr/todd/ecommerce/security/WebSecurityConfig.java
@@ -15,8 +15,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] PUBLIC_MATCHERS = new String[]{"/", "/products", "/products/**"};
 
+    @Autowired
     private final UserDetailsService userDetailsService;
 
+    @Autowired
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
@@ -34,6 +36,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated()
+//            .antMatchers(MATCHER_PAGE_INACCESSIBLE).denyAll()
+//            .antMatchers(MATCHER_PAGE_USER_AUTHENTIFIE_REVENU_AVEC_COOKIE).rememberMe()
+//            .anyRequest().authenticated() // toutes les autres
                 .and()
                 .formLogin()
 //                .loginPassword("/login")
